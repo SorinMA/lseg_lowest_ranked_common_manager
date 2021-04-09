f1c00ccb2bc908174bc1b1c86332bfbf3826a334
SorinMA <sorinmartinescu9dev@gmail.com>
2021-04-09 18:14:57 +0300

    the project is initialized

---
+++ src/main/java/com/lseg/lowest_ranked_common_manager/LowestRankedCommonManagerApplication.java
@@ -0,0 +1,9 @@
+package com.lseg.lowest_ranked_common_manager;
+
+public class LowestRankedCommonManagerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
